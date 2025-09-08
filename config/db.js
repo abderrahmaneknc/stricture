ed386f5093c813d8b3bbc5ab940a7eb1adce7023
kennoucheAbdErrahmane <a.kennouche@esi-sba.dz>
2025-09-08 09:08:48 +0100

    db connection

---
+++ config/db.js
@@ -0,0 +1,20 @@
+//connection with datatbase
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+dotenv.config();
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    logging: false, // optional, disables SQL logging
+  }
+);
+
+
+
+
+module.exports = sequelize;
