12eded2a2f2a2717a77bdcc61fc7464bce303ca2
abderrahmaneknc <148714671+abderrahmaneknc@users.noreply.github.com>
2025-11-25 16:36:16 +0100

    Refactor app.js for improved structure and clarity

--- app.js
+++ app.js
@@ -2,12 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
-const sequelize = require('./config/db');
-
-dotenv.config();
-
 const app = express();
-
+dotenv.config();
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,13 +13,13 @@ app.use('/api/auth', authRoutes);
 
 // Default route
 app.get('/', (req, res) => {
-    res.send('API is running');
+  res.send('API is running');
 });
 
-// Global error handler (optional)
+// Global error handler
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Server Error' });
+  console.error(err.stack);
+  res.status(500).json({ message: 'Server Error' });
 });
 
 module.exports = app;
